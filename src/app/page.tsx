07d1a335e164b0e9d30fa66c1c03ce2093dae7db
Asma Akbar <masoomlarki297@gmail.com>
2024-10-29 10:37:30 +0500

    New change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,8 @@ export default
 function Home(){
   return(
 <div className=" text-white text-center py-64 font-semibold text-5xl">      
-      <h1  >Hello world</h1>
+      <h1  >Hello world..!</h1>
+
 
       
     </div>
