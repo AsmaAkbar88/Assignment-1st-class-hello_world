5e8332f8446fc93110626060c4edc8210d510ae1
Asma Akbar <masoomlarki297@gmail.com>
2024-10-29 15:09:42 +0500

    New add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 export default
 function Home(){
   return(
-    <div>
-      <center>
-      <h1>Hello world</h1>
-  </center>
+    <div className=" bg-black text-white flex justify-center items-center font-semibold text-5xl py-72 underline">
+      
+      <h1 >Hello world</h1>
+  
       
     </div>
   )
